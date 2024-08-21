2eb75716255b05ab533ad361668ab39b1db6d7c4
G <jayendra.g@sap.com>
2024-08-21 14:35:24 +0530

    re organizing the files

---
+++ chrome-extension/report.js
@@ -0,0 +1,30 @@
+const host = "https://psi-ext.onrender.com/";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const urlInput = document.getElementById("urlInput");
+  const reportDiv = document.getElementById("report");
+  const getReportButton = document.getElementById("getReportButton");
+
+  getReportButton.addEventListener("click", async () => {
+    const url = urlInput.value;
+
+    reportDiv.textContent = "Fetching report...";
+
+    try {
+      const response = await fetch(host + "/report", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      });
+
+      const data = await response.json();
+
+      reportDiv.textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      reportDiv.textContent = "Failed to fetch report. Please try again.";
+    }
+  });
+});
